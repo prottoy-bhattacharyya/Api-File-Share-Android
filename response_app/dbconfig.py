03d578d4468867df5155db1850afe05f99bc7b34
Prottoy Vhattacharyya <prottoybhattacharya@gmail.com>
2025-11-22 12:30:39 +0600

    dbconfig set for environment var

--- response_app/dbconfig.py
+++ response_app/dbconfig.py
@@ -1,9 +1,11 @@
+import os
+
 def db():
     config = {
-        'host': 'localhost',
-        'user': 'root',
-        'password': '',
-        'port': '3306',
-        'database': 'file_share_db'
+        'host': os.environ.get('DB_HOST'),
+        'user': os.environ.get('DB_USER'),
+        'password': os.environ.get('DB_PASSWORD'),
+        'port': os.environ.get('DB_PORT'),
+        'database': os.environ.get('DB_NAME')
     }
     return config
