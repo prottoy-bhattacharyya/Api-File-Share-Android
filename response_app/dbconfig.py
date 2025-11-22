8aadd5dcd8ddf784f28d102dd86889d90d6aac4a
Prottoy Vhattacharyya <prottoybhattacharya@gmail.com>
2025-11-22 12:17:47 +0600

    api created

---
+++ response_app/dbconfig.py
@@ -0,0 +1,9 @@
+def db():
+    config = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': '',
+        'port': '3306',
+        'database': 'file_share_db'
+    }
+    return config
